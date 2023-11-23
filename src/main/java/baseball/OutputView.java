25b98eb63ae06c9432ae78f8a737b0445045f50b
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-11-23 18:03:53 +0900

    feat : 게임 재시작 여부 입력 안내 문구 출력

--- src/main/java/baseball/OutputView.java
+++ src/main/java/baseball/OutputView.java
@@ -24,4 +24,9 @@ public class OutputView {
         }
         System.out.println(output);
     }
+
+    public void printNewGameStartOrNot() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
+                                   + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
