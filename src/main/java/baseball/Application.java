a402b260752b649505dfc06b73b00671101a4622
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-11-23 18:04:54 +0900

    feat : 숫자 야구 게임 메인 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import java.util.List;
+import java.util.Map;
+
 public class Application {
+
+    static int STOP = 2;
+    static InputView inputView = new InputView();
+    static OutputView outputView = new OutputView();
+    static Game game = new Game();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        outputView.printGameStart();
+        do {
+            Computer computer = new Computer();
+            userTurn(computer);
+            outputView.printNewGameStartOrNot();
+        } while (inputView.readNewGameSignal() != STOP);
+
+    }
+
+    private static void userTurn(Computer computer) {
+        do {
+            List<Integer> userNums = inputView.readUserNums();
+            User user = new User(userNums);
+
+            Map<State, Integer> result = game.game(computer.nums, user.nums);
+            outputView.printGameResult(result);
+        } while (game.gameContinue());
     }
 }
